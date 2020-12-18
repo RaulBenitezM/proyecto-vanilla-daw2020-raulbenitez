b5606b75c4d138ba691dabb44c3155050cad1305
RaulBenitezM <benitezmillet.raul2@gmail.com>
2020-12-18 01:53:05 +0100

    Actualización de ficheros 18/12/2020 - 01:53

--- scripts/index.js
+++ scripts/index.js
@@ -9,44 +9,6 @@ function mostrarLogin() {
     '<p><input type="text" id="usuario" name="usuario" /></p>';
 }
 
-// PRUEBAS PARA EL JSON CUESTIONARIO
-
-let fecha = new Date();
-
-let cuestionario = [
-  {
-    correo: 'raul@gmail.com',
-    fecha: fecha.toUTCString(),
-    preguntas: [
-      {
-        titulo: 'Pregunta 1',
-        respuesta: true,
-        puntuacion: 2,
-      },
-    ],
-  },
-];
-
-console.log(JSON.stringify(cuestionario));
-
-let persona = {
-  correo: 'raul2@gmail.com',
-  fecha: fecha.toUTCString(),
-  preguntas: [
-    {
-      titulo: 'Pregunta 1',
-      respuesta: false,
-      puntuacion: 4,
-    },
-  ],
-};
-
-cuestionario.push(persona);
-
-console.log(JSON.stringify(cuestionario));
-
-// FIN DE PRUEBA PARA EL JSON CUESTIONARIO
-
 //A los 5 segundos de cargar la página saldrá el Login (a menos que se pulse antes Ctrl + F10)
 let temp = setTimeout(() => mostrarLogin(), 5000);
 
@@ -62,17 +24,35 @@ window.addEventListener('blur', (event) => {
   let inputUsuario = document.getElementById('usuario');
 
   let pattern = /\S+@\S+\.\S+/;
+
   if (pattern.test(inputUsuario.value)) {
-    //let fecha = new Date();
-    //console.log(fecha.getHours());
-    //console.log(fecha.getMinutes());
-    //console.log(fecha.getSeconds());
-    //console.log(cookie);
-    //location.href = 'usuario.html';
+    let fecha = new Date();
+
+    let cuestionario = [
+      {
+        correo: inputUsuario.value,
+        fechaHora: {
+          day: fecha.getDay(),
+          month: fecha.getMonth(),
+          year: fecha.getFullYear(),
+          hour: fecha.getHours(),
+          minute: fecha.getMinutes(),
+          second: fecha.getSeconds(),
+        },
+        preguntas: [],
+      },
+    ];
+
+    strCuestionario = JSON.stringify(cuestionario);
+
+    Cookies.set('cuestionario', strCuestionario, { expires: 7 });
+
+    location.href = 'usuario.html';
   } else {
     let secError = document.getElementById('error');
     secError.innerHTML = '<p>El e-mail es incorrecto!</p>';
-    //TO-DO No funciona el foco al ser incorrecte
+
+    //TO-DO No funciona el foco al ser incorrecto
     setTimeout(() => {
       inputUsuario.focus();
       inputUsuario.select();
---
+++ scripts/usuario.js
@@ -0,0 +1,22 @@
+//TO-DO Poner comentarios
+let cuestionario = JSON.parse(Cookies.get('cuestionario'));
+
+let p1 = document.getElementById('p1');
+
+let p2 = document.getElementById('p2');
+
+p1.innerHTML = 'Hola ' + cuestionario[0].correo;
+
+p2.innerHTML =
+  'La última vez que entraste fue el ' +
+  cuestionario[0].fechaHora.day +
+  '-' +
+  cuestionario[0].fechaHora.month +
+  '-' +
+  cuestionario[0].fechaHora.year +
+  ' a las ' +
+  cuestionario[0].fechaHora.hour +
+  ':' +
+  cuestionario[0].fechaHora.minute +
+  ':' +
+  cuestionario[0].fechaHora.second;
